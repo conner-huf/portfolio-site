67670cfc47287ef7b38e1eff5953a9f63051ae90
conner-huf <conner.huf@gmail.com>
2023-11-09 12:31:15 -0800

    added bg, social icons, and some styling

--- src/Components/Header/Logo/Logo.jsx
+++ src/Components/Header/Logo/Logo.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import './Logo.css'
+import { SocialIcon } from 'react-social-icons'
 
 export const Logo = () => {
   return (
     // Here for if I ever make a decent "logo" for myself
-    <div> 
+    <div className='socials-container'>
+      <SocialIcon className="social-icon" bgColor="black" url="https://www.linkedin.com/in/conner-hufnagel-070243287/" target="_blank"/>
+      <SocialIcon className="social-icon" bgColor='black' url="https://github.com/conner-huf" target="_blank"/>
     </div>
   )
 }
