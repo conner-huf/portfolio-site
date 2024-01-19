8b90966da2b185764d5958b259df0b506c6d7334
conner-huf <conner.huf@gmail.com>
2024-01-19 12:57:11 -0800

    added mobile support
    added mobile support so the site works generally well on mobile. Still lots of improvements to make, like dialing in the styling and making the project card layers work better.

--- src/Components/Header/Navbar/Navbar.jsx
+++ src/Components/Header/Navbar/Navbar.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import { Link } from 'react-router-dom'
 import resume from '../../../Assets/Resume.pdf'
 
 export const Navbar = () => {
+
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <ul>
-        <li><Link to='/' className="nav-link">Home</Link></li>
-        <li><Link to='/about' className="nav-link">About Me</Link></li>
-        <li><Link to='projects' className="nav-link">Projects</Link></li>
-        <li><a href={resume} download className="nav-link">Resume</a></li>
-    </ul>
+    <nav>
+      <div className="menu-container">
+        <div className="hamburger" onClick={() => setIsOpen(!isOpen)}>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <ul className={isOpen ? 'nav-links open' : 'nav-links'}>
+          <li><Link to='/' className="nav-link">Home</Link></li>
+          <li><Link to='/about' className="nav-link">About Me</Link></li>
+          <li><Link to='projects' className="nav-link">Projects</Link></li>
+          <li><a href={resume} download className="nav-link">Resume</a></li>
+        </ul>
+      </div>
+    </nav>
   )
 }
