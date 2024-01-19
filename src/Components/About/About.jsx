5316845109b653f662712313ab4e327c97601f96
conner-huf <conner.huf@gmail.com>
2024-01-19 13:40:34 -0800

    fixed project display and about me scrolling

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -4,10 +4,10 @@ import profilePic from '../../Assets/ProfilePic.png';
 
 export const About = () => {
   return (
-    <div className='about-content'>
-        <aside className='about-section1'>
+    <div className='about-page'>
+        <aside className='about-text-section'>
             <h1 className='greeting'>Hi!</h1>
-            <p className='about-me'>
+            <p className='about-me-text'>
               My name's Conner. I'm a software developer based in Los Angeles, CA.
               <br/><br/>
               I specialize in developing React applications, and I specifically enjoy developing tools related to video games, movies, music, etc.
@@ -17,7 +17,7 @@ export const About = () => {
               If you'd like to get in touch, feel free to reach out to me using the LinkedIn icon above, or shoot me an email at conner.huf@gmail.com. You can also play any of my finished Unity projects by using the Itch.io link above.
             </p>
         </aside>
-        <aside className='about-section2'>
+        <aside className='about-pic-section'>
             <img src={profilePic} alt="" className='profilePic'></img>
         </aside>
     </div>
