6220883e8a518cc2fa4befb099aa67ead46910c6
Conner Hufnagel <conner.huf@gmail.com>
2024-02-03 16:20:10 -0800

    style update

--- src/Components/Header/Logo/Logo.jsx
+++ src/Components/Header/Logo/Logo.jsx
@@ -6,9 +6,9 @@ export const Logo = () => {
   return (
     // Here for if I ever make a decent "logo" for myself
     <div className='socials-container'>
-      <SocialIcon className="social-icon" bgColor="black" url="https://www.linkedin.com/in/conner-hufnagel-070243287/" target="_blank"/>
-      <SocialIcon className="social-icon" bgColor='black' url="https://github.com/conner-huf" target="_blank"/>
-      <SocialIcon className="social-icon" bgColor="black" network="itch.io" url="https://itzlumpz.itch.io/" target="_blank"/>
+      <SocialIcon title="LinkedIn" className="social-icon" bgColor="black" url="https://www.linkedin.com/in/conner-hufnagel-070243287/" target="_blank"/>
+      <SocialIcon title="Github" className="social-icon" bgColor='black' url="https://github.com/conner-huf" target="_blank"/>
+      <SocialIcon title="Itch.io" className="social-icon" bgColor="black" network="itch.io" url="https://itzlumpz.itch.io/" target="_blank"/>
     </div>
   )
 }
