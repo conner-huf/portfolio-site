507cf1e99852f5ef66b2bd4f28ab16ad1327b1a2
Conner Hufnagel <conner.huf@gmail.com>
2024-03-22 15:03:20 -0700

    added a scroll to projects
    made it so the project display scrolls when it's mounted so the user can tell they can scroll it horizontally.

--- src/Components/ProjectDisplay/ProjectDisplay.jsx
+++ src/Components/ProjectDisplay/ProjectDisplay.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, {useEffect, useRef } from 'react'
 import './ProjectDisplay.css'
 import { ProjectCard } from '../ProjectCard/ProjectCard'
 
 import projects from '../../Assets/projects.json'
 
 export const ProjectDisplay = () => {
+
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const container = containerRef.current;
+    if (container) {
+      // Scroll a small amount to the right
+      container.scrollTo({ left: 500, behavior: 'smooth' });
+
+      // After 1 second, scroll back to the start
+      setTimeout(() => {
+        container.scrollTo({ left: 0, behavior: 'smooth' });
+      }, 500);
+    }
+  }, []);
+
   return (
-    <div className='project-backdrop'>
+    <div className='project-backdrop' ref={containerRef}>
       {projects.map((project, index) => (
         <ProjectCard key={index} index={index} project={project} />
       ))}
