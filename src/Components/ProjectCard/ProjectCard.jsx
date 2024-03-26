32d77abdd0d507a25d2e46e7f317aad82362fe2c
Conner Hufnagel <conner.huf@gmail.com>
2024-03-26 12:48:17 -0700

    update project order

--- src/Components/ProjectCard/ProjectCard.jsx
+++ src/Components/ProjectCard/ProjectCard.jsx
@@ -7,7 +7,7 @@ import teenyLinkPic from '../../Assets/project-teenylink.png'
 import asteroidsPic from '../../Assets/project-asteroids.png'
 import evermanaPic from '../../Assets/project-wow-addon.png'
 import pokedexPic from '../../Assets/project-pokedex.png'
-import spotifyPIc from '../../Assets/project-spotify-clone.png'
+import spotifyPic from '../../Assets/project-spotify-clone.png'
 import restaurantPic from '../../Assets/project-restaurant-site.png'
 
 const images = {
@@ -17,7 +17,7 @@ const images = {
     "Asteroids Game": asteroidsPic,
     "Evermana WoW Addon": evermanaPic,
     "Pokedex Application": pokedexPic,
-    "Spotify Clone": spotifyPIc,
+    "Spotify Clone": spotifyPic,
     "Restaurant Site": restaurantPic,
 }
 
